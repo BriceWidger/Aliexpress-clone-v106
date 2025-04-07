773afefc611ae1e09c73b1a675dea2b5d2189e34
Brice Widger <bkw11@rocketmail.com>
2025-04-07 19:03:32 -0400

    modified:   last_left_off.txt modified:   resources/JS/showAdded.js

--- resources/JS/showAdded.js
+++ resources/JS/showAdded.js
@@ -1,86 +1,92 @@
 /**
  * FOR ALL PAGES
- * 
- * Show an "Added!" notification on the given element.
- * The notification will be displayed for a duration of 1000 milliseconds. The user will not be able to click the product imgs during the function duration using the overlay wrapper.
  *
+ * Show an "Added!" notification on the given element.
+ * The notification will be displayed for a duration of 1000 milliseconds. Pointer events will be disabled during this time.
  */
 
 // Add-on icons (all)
 function showAdded(element) {
-  const wrapper = element.closest('.page-container'); // the wrapper is the whole page container to engulf all add-on icons and their pictures (verify the size of the .page-container by changing the background to a color)
+  const wrapper =
+    element.closest(".add-on-icon-wrapper") ||
+    element.closest(".page-container") ||
+    element.closest(".product-page-body-wrap");
 
-  if (wrapper) {
-    const overlay = document.createElement('div');
-    overlay.style.position = 'absolute';
-    overlay.style.top = '0';
-    overlay.style.left = '0';
-    overlay.style.width = '100%';
-    overlay.style.height = '100%';
-    overlay.style.background = 'transparent'; 
-    overlay.style.zIndex = '1';
-    overlay.style.cursor = 'default';
-    wrapper.appendChild(overlay);
-
-    overlay.addEventListener('click', function (event) {
-      event.stopPropagation();
-    });
-
-    overlay.addEventListener('click', function (event) {
-      event.preventDefault();
-    });
-
-    element.style.backgroundColor = 'lightgreen';
-    element.style.backgroundImage = 'none';
-    element.querySelector('.notification-text').innerText = "Added!";
-
-    setTimeout(() => {
-      element.querySelector('.notification-text').innerText = "";
-      element.style.backgroundImage = 'url("/resources/images/plus-icon-black.png")';
-      element.style.backgroundColor = '';
-      wrapper.removeChild(overlay);
-    }, 1000);
-  } else {
-    console.error('Could not find .add-on-icon-wrapper element');
+  if (!wrapper) {
+    console.error("Could not find a valid wrapper element");
+    return;
   }
-}
 
-/**
- *
- *
- *
- */
+  // Disable pointer events and set cursor to default
+  document.body.style.pointerEvents = "none";
+  document.body.style.cursor = "default";
+
+  const overlay = document.createElement("div");
+  Object.assign(overlay.style, {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    background: "transparent",
+    zIndex: "1",
+    cursor: "default",
+  });
+  wrapper.appendChild(overlay);
+
+  overlay.addEventListener("click", (event) => event.preventDefault());
+
+  element.style.backgroundColor = "lightgreen";
+  element.style.backgroundImage = "none";
+  element.querySelector(".notification-text").innerText = "Added!";
+
+  setTimeout(() => {
+    element.querySelector(".notification-text").innerText = "";
+    element.style.backgroundImage =
+      'url("/resources/images/plus-icon-black.png")';
+    element.style.backgroundColor = "";
+    if (wrapper.contains(overlay)) wrapper.removeChild(overlay);
+
+    // Re-enable pointer events and reset cursor
+    document.body.style.pointerEvents = "auto";
+    document.body.style.cursor = "pointer";
+  }, 1000);
+}
 
 /**
  * FOR PRODUCT PAGES
  *
- * Show an "Added!" notification on the given element.
- * The notification will be displayed for a duration of 1000 milliseconds for the "Add to Cart" button.
- *
- * @param {HTMLElement} element - The element on which the notification will be displayed.
+ * Show an "Added!" notification on the "Add to Cart" button.
+ * Pointer events will be disabled during the notification duration.
  */
-
-// "Add to Cart" button (product page)
 function showAdded_addToCartBtn(element) {
-  element.style.backgroundColor = 'lightgreen';
-  element.style.backgroundImage = 'none';
-  element.style.pointerEvents = 'none';
+  // Disable pointer events and set cursor to default
+  document.body.style.pointerEvents = "none";
+  document.body.style.cursor = "default";
+
+  element.style.backgroundColor = "lightgreen";
+  element.style.backgroundImage = "none";
 
   setTimeout(() => {
-    element.querySelector('.notification-text-add-to-cart-button').innerText = "";
-    element.style.backgroundImage = 'url("/resources/images/plus-icon-black.png")';
-    element.style.backgroundColor = '';
-    element.style.pointerEvents = 'auto';
+    element.querySelector(".notification-text-add-to-cart-button").innerText =
+      "";
+    element.style.backgroundImage =
+      'url("/resources/images/plus-icon-black.png")';
+    element.style.backgroundColor = "";
+
+    // Re-enable pointer events and reset cursor
+    document.body.style.pointerEvents = "auto";
+    document.body.style.cursor = "pointer";
   }, 1000);
 }
 
-const addToCart_textChange = document.querySelector('.add-to-cart-wrap');
-
-addToCart_textChange.addEventListener('click', function () {
-  const addToCartText = addToCart_textChange.querySelector('.add-to-cart-text');
-  addToCartText.textContent = 'Added!';
+// Update "Add to Cart" button text
+const addToCart_textChange = document.querySelector(".add-to-cart-wrap");
+addToCart_textChange.addEventListener("click", () => {
+  const addToCartText = addToCart_textChange.querySelector(".add-to-cart-text");
+  addToCartText.textContent = "Added!";
 
   setTimeout(() => {
-    addToCartText.textContent = 'Add to Cart';
+    addToCartText.textContent = "Add to Cart";
   }, 1000);
 });
